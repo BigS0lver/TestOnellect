b553f7fd99e078cbdbf8ad8e6e9d8e5ec5a26eff
MLGman Morgan <zhekas2011@hotmail.com>
2024-05-28 00:52:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace TestOnellect
+{
+    public class Program
+    {
+        private static readonly HttpClient httpClient = new();
+        static void Main(string[] args)
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                var url = config.GetSection("Url").Value;
+
+                Random r = new();
+                SortMethods sortMethods = new();
+                var list = new List<int>();
+                var count = r.Next(20, 101);
+
+                for (var i = 0; i < count; i++)
+                    list.Add(r.Next(-100, 100));
+
+                list.ForEach(num => Console.Write(num + " "));
+                Console.WriteLine();
+
+                sortMethods.RandomSort(list);
+                list.ForEach(num => Console.Write(num + " "));
+                Console.WriteLine();
+
+                StringContent jsonContent = new(
+                    JsonSerializer.Serialize(list),
+                    Encoding.UTF8,
+                    "application/json");
+
+                using var request = new HttpRequestMessage(HttpMethod.Post, url);
+                request.Content = jsonContent;
+                using var response = httpClient.Send(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ SortMethods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOnellect
+{
+    public class SortMethods
+    {
+        public void BubbleSort(List<int> array)
+        {
+            for (var i = 0; i < array.Count - 1; i++)
+                for (var j = 0; j < array.Count - i - 1; j++)
+                    if (array[j] > array[j + 1])
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+        }
+
+        public void QuickSort(List<int> array, int leftIndex, int rightIndex)
+        {
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = array[leftIndex];
+            while (i <= j)
+            {
+                while (array[i] < pivot) i++;
+
+                while (array[j] > pivot) j--;
+
+                if (i <= j)
+                {
+                    (array[i], array[j]) = (array[j], array[i]);
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                QuickSort(array, leftIndex, j);
+            if (i < rightIndex)
+                QuickSort(array, i, rightIndex);
+        }
+
+        public void QuickSort(List<int> array)
+        {
+            QuickSort(array, 0, array.Count - 1);
+        }
+
+        public void RandomSort(List<int> array)
+        {
+            SortMethod[] sortMethods = [QuickSort, BubbleSort];
+
+            var r = new Random();
+            var randomMethod = sortMethods[r.Next(sortMethods.Length)];
+            Console.WriteLine(randomMethod.Method.Name);
+            randomMethod(array);
+        }
+
+        private delegate void SortMethod(List<int> arr);
+    }
+}
